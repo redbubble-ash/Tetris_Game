bf74b391bb757359e016f358ff3d167cae36b500
Jing Xie <xiejing418@gmail.com>
2019-11-01 10:52:14 -0700

    fixed the coordinates for each shapes

--- Piece.cs
+++ Piece.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Tetris_Game
 
@@ -14,13 +15,13 @@ namespace Tetris_Game
         {
 
             pieces = new Point[7][];
-            pieces[0] = new Point[] { new Point { x = 1, y = 3 }, new Point { x = 1, y = 2 }, new Point { x = 1, y = 1 }, new Point { x = 1, y = 0 } }; // I shape
-            pieces[1] = new Point[] { new Point { x = 0, y = 0 }, new Point { x = 1, y = 0 }, new Point { x = 0, y = 1 }, new Point { x = 1, y = 1 } }; // O shape
-            pieces[2] = new Point[] { new Point { x = 0, y = 1 }, new Point { x = 1, y = 0 }, new Point { x = 1, y = 1 }, new Point { x = 2, y = 0 } }; // T shape
-            pieces[3] = new Point[] { new Point { x = 0, y = 0 }, new Point { x = 1, y = 0 }, new Point { x = 1, y = 1 }, new Point { x = 2, y = 1 } }; // S shape
-            pieces[4] = new Point[] { new Point { x = 0, y = 1 }, new Point { x = 1, y = 0 }, new Point { x = 1, y = 1 }, new Point { x = 2, y = 0 } }; // Z shape
-            pieces[5] = new Point[] { new Point { x = 0, y = 0 }, new Point { x = 1, y = 0 }, new Point { x = 2, y = 0 }, new Point { x = 2, y = 1 } }; // J shape
-            pieces[6] = new Point[] { new Point { x = 0, y = 0 }, new Point { x = 0, y = 1 }, new Point { x = 1, y = 0 }, new Point { x = 2, y = 0 } }; // L shape
+            pieces[0] = new Point[] { new Point { x = 1, y = 0 }, new Point { x = 1, y = 1 }, new Point { x = 1, y = 2 }, new Point { x = 1, y = 3 } }; // I shape
+            pieces[1] = new Point[] { new Point { x = 1, y = 0 }, new Point { x = 1, y = 1 }, new Point { x = 2, y = 0 }, new Point { x = 2, y = 1 } }; // O shape
+            pieces[2] = new Point[] { new Point { x = 0, y = 0 }, new Point { x = 1, y = 0 }, new Point { x = 1, y = 1 }, new Point { x = 2, y = 0 } }; // T shape
+            pieces[3] = new Point[] { new Point { x = 0, y = 1 }, new Point { x = 1, y = 0 }, new Point { x = 1, y = 1 }, new Point { x = 2, y = 0 } }; // S shape
+            pieces[4] = new Point[] { new Point { x = 0, y = 0 }, new Point { x = 1, y = 0 }, new Point { x = 1, y = 1 }, new Point { x = 2, y = 1 } }; // Z shape
+            pieces[5] = new Point[] { new Point { x = 0, y = 0 }, new Point { x = 1, y = 0 }, new Point { x = 1, y = 1 }, new Point { x = 1, y = 2 } }; // J shape
+            pieces[6] = new Point[] { new Point { x = 0, y = 0 }, new Point { x = 0, y = 1 }, new Point { x = 0, y = 2 }, new Point { x = 1, y = 2 } }; // L shape
 
             //fill each shape into 4x4 matrix and assign filled cell to 1 (default the unfilled cells are zero)
             for (int i = 0; i < pieces.Length; i++)
@@ -44,10 +45,23 @@ namespace Tetris_Game
                 {
                     for (int y = 0; y < 4; y++)
                     {
-                        Console.Write(block[x, y] + " ");
+                        //Console.Write(block[x, y] + " ");
+                        if (block[y, x] == 1)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Yellow;
+                            Console.Write("  ");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Gray;
+                            Console.Write("  ");
+                            Console.ResetColor();
+                        }
                     }
+                    Console.WriteLine();
+
                 }
-                Console.WriteLine();
             }
         }
     }
