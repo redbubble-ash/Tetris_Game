debffba3f5823f016e92956bd3d015b8c86af97f
Jing Xie <xiejing418@gmail.com>
2019-11-01 10:08:00 -0700

    initialized 7 different shapes of blocks

--- Piece.cs
+++ Piece.cs
@@ -1,14 +1,17 @@
-﻿namespace Tetris_Game
+﻿using System;
+
+namespace Tetris_Game
+
 {
     class Piece
     {
         enum Bricks { I, O, T, S, Z, J, L }
 
         static Point[][] pieces;  // jagged arrays.
+        static int[][,] blocks = new int[7][,]; //arrary of 7 2d arrarys
 
         static Piece()
         {
-            int[,] block = new int[4, 4];
 
             pieces = new Point[7][];
             pieces[0] = new Point[] { new Point { x = 1, y = 3 }, new Point { x = 1, y = 2 }, new Point { x = 1, y = 1 }, new Point { x = 1, y = 0 } }; // I shape
@@ -20,17 +23,33 @@
             pieces[6] = new Point[] { new Point { x = 0, y = 0 }, new Point { x = 0, y = 1 }, new Point { x = 1, y = 0 }, new Point { x = 2, y = 0 } }; // L shape
 
             //fill each shape into 4x4 matrix and assign filled cell to 1 (default the unfilled cells are zero)
-            foreach (var piece in pieces)
+            for (int i = 0; i < pieces.Length; i++)
             {
-                foreach (var cell in piece)
-                {
-                    block[cell.x,cell.y] = 1;
-                }
-            }
+                int[,] block = new int[4, 4]; //signle 4x4 block
 
+                foreach (var cell in pieces[i])
+                {
+                    block[cell.x, cell.y] = 1;
+                }
+                blocks[i] = block; // each block represents a different shape
+
+            }
         }
 
+        public void listOfBlocks()
+        {
+            foreach (var block in blocks)
+            {
+                for (int x = 0; x < 4; x++)
+                {
+                    for (int y = 0; y < 4; y++)
+                    {
+                        Console.Write(block[x, y] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
     }
-
 
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace Tetris_Game
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Piece printShapes = new Piece();
+            Console.WriteLine("7 unique shapes are");
+            printShapes.listOfBlocks();
 
         }
 
